b5c93af31f15b1448d428dffdbe7a4af9fb8a092
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-01-15 12:46:50 -0500

    #cm: Not changing project version in script.

--- update-version.sh
+++ update-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Sets the project version and copies the data in S3.
+# Copies the data in S3.
 
 V=$1
 
-mvn versions:set -DnewVersion=$V -DgenerateBackupPoms=false
+#mvn versions:set -DnewVersion=$V -DgenerateBackupPoms=false
 aws s3 sync ./data/ s3://mtnfog-development/phileas-data/$V/
 
 
