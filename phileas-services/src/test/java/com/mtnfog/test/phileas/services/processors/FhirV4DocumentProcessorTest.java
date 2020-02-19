2ef1509621a6b88c1e192215dfdbb060c3b25c35
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-02-19 09:52:03 -0500

    PHI-72: Updating test.

--- phileas-services/src/test/java/com/mtnfog/test/phileas/services/processors/FhirV4DocumentProcessorTest.java
+++ phileas-services/src/test/java/com/mtnfog/test/phileas/services/processors/FhirV4DocumentProcessorTest.java
@@ -1,5 +1,7 @@
 package com.mtnfog.test.phileas.services.processors;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -12,10 +14,17 @@ import com.mtnfog.phileas.model.profile.fhir4.FhirR4;
 import com.mtnfog.phileas.model.responses.FilterResponse;
 import com.mtnfog.phileas.model.services.MetricsService;
 import com.mtnfog.phileas.services.processors.FhirV4DocumentProcessor;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.Patient;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.io.File;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 
@@ -35,7 +44,8 @@ public class FhirV4DocumentProcessorTest {
         final Structured structured = new Structured();
 
         final FhirR4 fhirR4 = new FhirR4();
-        fhirR4.setFhirItems(Arrays.asList(new FhirItem("patient.address.city", "DELETE")));
+        fhirR4.setFhirItems(Arrays.asList(new FhirItem("patient.name.family", "DELETE"), new FhirItem("patient.name.given", "DELETE")));
+        //fhirR4.setFhirItems(Arrays.asList(new FhirItem("patient.address.city", "DELETE")));
         structured.setFhirR4(fhirR4);
 
         filterProfile.setStructured(structured);
@@ -47,6 +57,33 @@ public class FhirV4DocumentProcessorTest {
         final FilterResponse filterResponse = documentProcessor.process(filterProfile, "context", "documentId", json);
 
         prettyPrintJson(filterResponse.getFilteredText());
+
+        // Parse the returned FHIR json.
+        final FhirContext ctx = FhirContext.forR4();
+        final IParser parser = ctx.newJsonParser();
+        final Bundle bundle = parser.parseResource(Bundle.class, filterResponse.getFilteredText());
+
+        for(final Bundle.BundleEntryComponent bundleEntryComponent : bundle.getEntry()) {
+
+            if(bundleEntryComponent.getResource() instanceof Patient) {
+
+                final Patient patient = (Patient) bundleEntryComponent.getResource();
+                for(HumanName humanName : patient.getName()) {
+
+                    System.out.println("Manipulated human name:");
+                    prettyPrintJson(humanName);
+
+                    System.out.println("Filtered human family name: " + humanName.getFamily());
+                    System.out.println("Filtered human given name: " + humanName.getGiven().size());
+
+                    Assert.assertEquals("", humanName.getFamily());
+                    Assert.assertEquals(true, humanName.getGiven().isEmpty());
+
+                }
+
+            }
+
+        }
 
     }
 
