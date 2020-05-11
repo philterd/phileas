74d23472d8e48d9d785871f7b79730df9fb9d5ef
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-05-11 14:36:16 -0400

    PHL-83: Fixing redis test.

--- phileas-services/src/test/java/com/mtnfog/test/phileas/services/disambiguation/RedisVectorBasedSpanDisambiguationServiceTest.java
+++ phileas-services/src/test/java/com/mtnfog/test/phileas/services/disambiguation/RedisVectorBasedSpanDisambiguationServiceTest.java
@@ -3,6 +3,9 @@ package com.mtnfog.test.phileas.services.disambiguation;
 import com.mtnfog.phileas.model.enums.FilterType;
 import com.mtnfog.phileas.model.objects.Span;
 import com.mtnfog.phileas.services.disambiguation.VectorBasedSpanDisambiguationService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,6 +17,8 @@ import java.util.List;
 import java.util.Properties;
 
 public class RedisVectorBasedSpanDisambiguationServiceTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(RedisVectorBasedSpanDisambiguationServiceTest.class);
 
     private RedisServer redisServer;
 
@@ -35,18 +40,7 @@ public class RedisVectorBasedSpanDisambiguationServiceTest {
     @Test
     public void disambiguateWithRedis1() {
 
-        final Properties properties = new Properties();
-
-        properties.setProperty("span.disambiguation.enabled", "true");
-        properties.setProperty("span.disambiguation.ignore.stopwords", "false");
-        properties.setProperty("span.disambiguation.vector.size", "32");
-
-        properties.setProperty("cache.redis.enabled", "true");
-        properties.setProperty("cache.redis.host", "localhost");
-        properties.setProperty("cache.redis.port", "31000");
-        properties.setProperty("cache.redis.ssl", "false");
-        properties.setProperty("cache.redis.auth.token", "");
-        properties.setProperty("cache.redis.cluster", "false");
+        final Properties properties = getProperties();
 
         final String context = "c";
 
@@ -67,6 +61,48 @@ public class RedisVectorBasedSpanDisambiguationServiceTest {
         final FilterType filterType = vectorBasedSpanDisambiguationService.disambiguate(context, filterTypes, ambiguousSpan);
 
         Assert.assertEquals(FilterType.PHONE_NUMBER, filterType);
+
+    }
+
+    private Properties getProperties() {
+
+        final Properties properties = new Properties();
+
+        properties.setProperty("span.disambiguation.enabled", "true");
+        properties.setProperty("span.disambiguation.ignore.stopwords", "false");
+        properties.setProperty("span.disambiguation.vector.size", "32");
+
+        properties.setProperty("cache.redis.enabled", "true");
+
+        final String redisHost = System.getenv("PHILTER_REDIS_HOST");
+        final String redisPort = System.getenv("PHILTER_REDIS_PORT");
+        final String redisSsl = System.getenv("PHILTER_REDIS_SSL");
+        final String redisToken = System.getenv("PHILTER_REDIS_AUTH_TOKEN");
+        final String redisClustered = System.getenv("PHILTER_REDIS_CLUSTERED");
+
+        if(StringUtils.isNotEmpty(redisHost)) {
+
+            LOGGER.info("Using redis host: {}", redisHost);
+
+            properties.setProperty("cache.redis.host", redisHost);
+            properties.setProperty("cache.redis.port", redisPort);
+            properties.setProperty("cache.redis.ssl", redisSsl);
+            properties.setProperty("cache.redis.auth.token", redisToken);
+            properties.setProperty("cache.redis.cluster", redisClustered);
+
+        } else {
+
+            LOGGER.info("Using local redis host.");
+
+            properties.setProperty("cache.redis.host", "localhost");
+            properties.setProperty("cache.redis.port", "31000");
+            properties.setProperty("cache.redis.ssl", "false");
+            properties.setProperty("cache.redis.auth.token", "");
+            properties.setProperty("cache.redis.cluster", "false");
+
+        }
+
+        return properties;
 
     }
 
