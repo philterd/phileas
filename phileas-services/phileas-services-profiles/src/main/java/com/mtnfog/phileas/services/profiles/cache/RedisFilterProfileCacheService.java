93774fb0d01a765b0416977d15c4c2b4324e1e3b
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-05-29 13:01:29 -0400

    #sonarqube: Fixing sonar issues.

--- phileas-services/phileas-services-profiles/src/main/java/com/mtnfog/phileas/services/profiles/cache/RedisFilterProfileCacheService.java
+++ phileas-services/phileas-services-profiles/src/main/java/com/mtnfog/phileas/services/profiles/cache/RedisFilterProfileCacheService.java
@@ -17,15 +17,19 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
 
     private static final Logger LOGGER = LogManager.getLogger(RedisFilterProfileCacheService.class);
 
+    private static final String CACHE_KEY = "filterProfiles";
+    
     public RedisFilterProfileCacheService(PhileasConfiguration phileasConfiguration) throws IOException {
         super(phileasConfiguration);
+
+        LOGGER.info("Initializing Reds filter profile cache.");
     }
 
     @Override
     public List<String> get() {
 
         // Get the names from the cache and return them.
-        final RMap<String, String> names = redisson.getMap("filterProfiles");
+        final RMap<String, String> names = redisson.getMap(CACHE_KEY);
 
         return new ArrayList<>(names.keySet());
 
@@ -35,7 +39,7 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
     public String get(String filterProfileName) {
 
         // Get the filter profile from the cache and return it.
-        final RMap<String, String> map = redisson.getMap("filterProfiles");
+        final RMap<String, String> map = redisson.getMap(CACHE_KEY);
 
         return map.get(filterProfileName);
 
@@ -44,12 +48,12 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
     @Override
     public Map<String, String> getAll() {
 
-        final long count = redisson.getKeys().countExists("filterProfiles");
+        final long count = redisson.getKeys().countExists(CACHE_KEY);
 
         if(count != 0) {
 
             // Get the filter profiles from the cache and return them.
-            final RMap<String, String> map = redisson.getMap("filterProfiles");
+            final RMap<String, String> map = redisson.getMap(CACHE_KEY);
 
             Map<String, String> m = new HashMap<>();
             for(String k : map.keySet()) {
@@ -70,7 +74,7 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
     public void insert(String filterProfileName, String filterProfile) {
 
         // Insert into the the cache.
-        final RMap<String, String> map = redisson.getMap("filterProfiles");
+        final RMap<String, String> map = redisson.getMap(CACHE_KEY);
         map.put(filterProfileName, filterProfile);
 
     }
@@ -79,7 +83,7 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
     public void remove(String filterProfileName) {
 
         // Remove from the cache.
-        final RMap<String, String> map = redisson.getMap("filterProfiles");
+        final RMap<String, String> map = redisson.getMap(CACHE_KEY);
         map.remove(filterProfileName);
 
     }
@@ -88,7 +92,7 @@ public class RedisFilterProfileCacheService extends AbstractRedisCacheService im
     public void clear() {
 
         // Clear the cache.
-        final RMap<String, String> map = redisson.getMap("filterProfiles");
+        final RMap<String, String> map = redisson.getMap(CACHE_KEY);
         map.clear();
 
     }
