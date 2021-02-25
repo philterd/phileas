10c48a77c51620c17fbc831a76a0b304309eb614
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-02-25 11:27:48 -0500

    PHL-196: Adding JSON deserialization for LAPPS json format.

---
+++ phileas-model/src/main/java/com/mtnfog/phileas/model/formats/lapps/Lapps.java
@@ -0,0 +1,57 @@
+package com.mtnfog.phileas.model.formats.lapps;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Lapps {
+
+    @SerializedName("@context")
+    @Expose
+    private String context;
+
+    @SerializedName("metadata")
+    @Expose
+    private Metadata metadata;
+
+    @SerializedName("text")
+    @Expose
+    private Text text;
+
+    @SerializedName("views")
+    @Expose
+    private List<View> views = null;
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    public Text getText() {
+        return text;
+    }
+
+    public void setText(Text text) {
+        this.text = text;
+    }
+
+    public List<View> getViews() {
+        return views;
+    }
+
+    public void setViews(List<View> views) {
+        this.views = views;
+    }
+
+}
