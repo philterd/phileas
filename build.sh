96157445d9e24d3b61e2cf9e3fc65c397d5ae498
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-07-21 17:29:21 -0400

    Changing gocd.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain mtnfog --domain-owner 341239660749 --query authorizationToken --output text --profile codeartifact`
+echo $CODEARTIFACT_AUTH_TOKEN
 mvn clean install
