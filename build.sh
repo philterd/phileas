90c5f2ea4da05da9868bc4a432379e30d2aec2f7
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-07-21 20:59:43 -0400

    Updating build step.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain mtnfog --domain-owner 341239660749 --query authorizationToken --output text`
 echo $CODEARTIFACT_AUTH_TOKEN
-mvn clean install
+mvn clean license:aggregate-add-third-party license:aggregate-download-licenses install deploy -Pit
