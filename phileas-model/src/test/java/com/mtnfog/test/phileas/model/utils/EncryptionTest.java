a40d9d35365576a7664aa1110dc8c82575888c0e
jzonthemtn <jeff.zemerick@mtnfog.com>
2022-07-19 10:19:01 -0400

    PHL-258: Adding unit test using the test vectors from NIST.

--- phileas-model/src/test/java/com/mtnfog/test/phileas/model/utils/EncryptionTest.java
+++ phileas-model/src/test/java/com/mtnfog/test/phileas/model/utils/EncryptionTest.java
@@ -1,6 +1,7 @@
 package com.mtnfog.test.phileas.model.utils;
 
 import com.mtnfog.phileas.model.profile.Crypto;
+import com.mtnfog.phileas.model.profile.FPE;
 import com.mtnfog.phileas.model.utils.Encryption;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,6 +14,45 @@ public class EncryptionTest {
 
     private static final String KEY = "9EE7A356FDFE43F069500B0086758346E66D8583E0CE1CFCA04E50F67ECCE5D1";
     private static final String IV = "B674D3B8F1C025AEFF8F6D5FA1AEAD3A";
+
+    @Test
+    public void formatPreservingEncryption1() {
+
+        // Test from https://csrc.nist.gov/csrc/media/projects/cryptographic-standards-and-guidelines/documents/examples/ff3samples.pdf
+
+        final String plainText = "890121234567890000";
+        final FPE fpe = new FPE("EF4359D8D580AA4F7F036D6F04FC6A94", "D8E7920AFA330A73");
+        final String encrypted = Encryption.formatPreservingEncrypt(fpe, plainText);
+
+        Assertions.assertEquals("750918814058654607", encrypted);
+
+    }
+
+    @Test
+    public void formatPreservingEncryption2() {
+
+        // Test from https://csrc.nist.gov/csrc/media/projects/cryptographic-standards-and-guidelines/documents/examples/ff3samples.pdf
+
+        final String plainText = "890121234567890000";
+        final FPE fpe = new FPE("EF4359D8D580AA4F7F036D6F04FC6A94", "9A768A92F60E12D8");
+        final String encrypted = Encryption.formatPreservingEncrypt(fpe, plainText);
+
+        Assertions.assertEquals("018989839189395384", encrypted);
+
+    }
+
+    @Test
+    public void formatPreservingEncryption3() {
+
+        // Test from https://csrc.nist.gov/csrc/media/projects/cryptographic-standards-and-guidelines/documents/examples/ff3samples.pdf
+
+        final String plainText = "89012123456789000000789000000";
+        final FPE fpe = new FPE("EF4359D8D580AA4F7F036D6F04FC6A94", "D8E7920AFA330A73");
+        final String encrypted = Encryption.formatPreservingEncrypt(fpe, plainText);
+
+        Assertions.assertEquals("48598367162252569629397416226", encrypted);
+
+    }
 
     @Test
     public void encrypt1() throws Exception {
