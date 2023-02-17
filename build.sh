dec976836daece43deab9b23f0d4ec8d4f3255d8
jzonthemtn <jeff.zemerick@mtnfog.com>
2023-02-17 11:24:53 -0500

    Changing to github packages.

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain mtnfog --domain-owner 341239660749 --query authorizationToken --output text`
-mvn clean license:aggregate-add-third-party license:aggregate-download-licenses install -Pit
