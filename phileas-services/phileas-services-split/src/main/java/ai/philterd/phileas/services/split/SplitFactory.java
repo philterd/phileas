c107414c8e75c2981a70242b3e87ffc644b53715
jzonthemtn <jeff.zemerick@philterd.ai>
2025-01-16 13:35:01 -0500

    Updating copyright dates to 2025.

--- phileas-services/phileas-services-split/src/main/java/ai/philterd/phileas/services/split/SplitFactory.java
+++ phileas-services/phileas-services-split/src/main/java/ai/philterd/phileas/services/split/SplitFactory.java
@@ -1,5 +1,5 @@
 /*
- *     Copyright 2024 Philterd, LLC @ https://www.philterd.ai
+ *     Copyright 2025 Philterd, LLC @ https://www.philterd.ai
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
