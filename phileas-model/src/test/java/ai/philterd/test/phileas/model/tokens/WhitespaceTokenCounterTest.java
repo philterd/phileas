2741c586e7938bfb2cc0788f85f50dbc0b2ef8e6
jzonthemtn <jeff.zemerick@philterd.ai>
2025-09-19 10:42:52 -0400

    Adding token counter and token counts to responses.

---
+++ phileas-model/src/test/java/ai/philterd/test/phileas/model/tokens/WhitespaceTokenCounterTest.java
@@ -0,0 +1,61 @@
+/*
+ *     Copyright 2025 Philterd, LLC @ https://www.philterd.ai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.philterd.test.phileas.model.tokens;
+
+import ai.philterd.phileas.model.tokens.TokenCounter;
+import ai.philterd.phileas.model.tokens.WhitespaceTokenCounter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WhitespaceTokenCounterTest {
+
+    @Test
+    public void testWhitespaceTokenCounter() {
+
+        final String input = "This is some sample text. It is here to test the token counter.";
+
+        final TokenCounter tokenCounter = new WhitespaceTokenCounter();
+        final long tokens = tokenCounter.countTokens(input);
+
+        Assertions.assertEquals(13, tokens);
+
+    }
+
+    @Test
+    public void testNoTokens() {
+
+        final String input = "";
+
+        final TokenCounter tokenCounter = new WhitespaceTokenCounter();
+        final long tokens = tokenCounter.countTokens(input);
+
+        Assertions.assertEquals(0, tokens);
+
+    }
+
+    @Test
+    public void testOneTokens() {
+
+        final String input = "word";
+
+        final TokenCounter tokenCounter = new WhitespaceTokenCounter();
+        final long tokens = tokenCounter.countTokens(input);
+
+        Assertions.assertEquals(1, tokens);
+
+    }
+
+}
