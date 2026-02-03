e117bf23f87f2f4d45c2c6d429f415d98c0b56a4
Jeff Zemerick <13176962+jzonthemtn@users.noreply.github.com>
2026-02-03 12:48:50 -0500

    Fixing text spans apply. (#292)

--- src/test/java/ai/philterd/phileas/services/filters/filtering/PlainTextFilterServiceTest.java
+++ src/test/java/ai/philterd/phileas/services/filters/filtering/PlainTextFilterServiceTest.java
@@ -22,7 +22,7 @@ public class PlainTextFilterServiceTest {
     private static final Logger LOGGER = LogManager.getLogger(PlainTextFilterServiceTest.class);
 
     @Test
-    public void apply1() throws Exception {
+    public void apply1() {
 
         final PhileasConfiguration phileasConfiguration = new PhileasConfiguration(new Properties());
         final ContextService contextService = new DefaultContextService();
@@ -34,13 +34,59 @@ public class PlainTextFilterServiceTest {
 
         final List<Span> spans = new ArrayList<>();
         spans.add(Span.make(0, 17, FilterType.PERSON, "context", 1.0, "George Washington", "***", "", false, true, null, 1));
-        spans.add(Span.make(18, 29, FilterType.SSN, "context", 1.0, "123-45-6789", "***", "", false, true, null, 1));
+        spans.add(Span.make(32, 43, FilterType.SSN, "context", 1.0, "123-45-6789", "***", "", false, true, null, 1));
 
         final byte[] bytes = service.apply(input.getBytes(StandardCharsets.UTF_8), spans);
         final String redacted = new String(bytes);
         LOGGER.info(redacted);
 
         Assertions.assertEquals("*** whose SSN was *** was the first president of the United States and he lived at 90210.", redacted);
+
+    }
+
+    @Test
+    public void apply2() {
+
+        final PhileasConfiguration phileasConfiguration = new PhileasConfiguration(new Properties());
+        final ContextService contextService = new DefaultContextService();
+        final VectorService vectorService = new InMemoryVectorService();
+
+        final PlainTextFilterService service = new PlainTextFilterService(phileasConfiguration, contextService, vectorService, null);
+
+        final String input = "George Washington was president and his SSN was 123-45-6789.";
+
+        final List<Span> spans = new ArrayList<>();
+        spans.add(Span.make(0, 17, FilterType.PERSON, "context", 1.0, "George Washington", "{{{REDACTED-person}}}", "", false, true, null, 1));
+        spans.add(Span.make(48, 59, FilterType.SSN, "context", 1.0, "123-45-6789", "{{{REDACTED-ssn}}}", "", false, true, null, 1));
+
+        final byte[] bytes = service.apply(input.getBytes(StandardCharsets.UTF_8), spans);
+        final String redacted = new String(bytes);
+        LOGGER.info(redacted);
+
+        Assertions.assertEquals("{{{REDACTED-person}}} was president and his SSN was {{{REDACTED-ssn}}}.", redacted);
+
+    }
+
+    @Test
+    public void apply3() {
+
+        final PhileasConfiguration phileasConfiguration = new PhileasConfiguration(new Properties());
+        final ContextService contextService = new DefaultContextService();
+        final VectorService vectorService = new InMemoryVectorService();
+
+        final PlainTextFilterService service = new PlainTextFilterService(phileasConfiguration, contextService, vectorService, null);
+
+        final String input = "George Washington was president and his SSN was 123-45-6789.";
+
+        final List<Span> spans = new ArrayList<>();
+        spans.add(Span.make(0, 17, FilterType.PERSON, "context", 1.0, "George Washington", "***", "", false, true, null, 1));
+        spans.add(Span.make(48, 59, FilterType.SSN, "context", 1.0, "123-45-6789", "***", "", false, true, null, 1));
+
+        final byte[] bytes = service.apply(input.getBytes(StandardCharsets.UTF_8), spans);
+        final String redacted = new String(bytes);
+        LOGGER.info(redacted);
+
+        Assertions.assertEquals("*** was president and his SSN was ***.", redacted);
 
     }
 
